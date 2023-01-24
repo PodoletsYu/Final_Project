00585cfeab90bb084a7dfe3eccc56889b69202ee
Zlaya_Ju <yulka-ne@list.ru>
2023-01-24 22:13:13 +0300

    решение задачи

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string[] arr1 = new string[7] {"Hello", "23", ":-)", "world", "Kazan", "home", "old"};
+string[] arr2 = new string[arr1.Length];
+
+void ResultArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ResultArray(arr1, arr2);
+PrintArray(arr2);
